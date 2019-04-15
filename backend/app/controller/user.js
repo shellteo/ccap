49db7e5b5a55c76321568bc63e016f0b71c3b1aa
张翔(zhangxiang) <shellteo@163.com>
2019-04-15 18:54:39 +0800

    update

--- backend/app/controller/user.js
+++ backend/app/controller/user.js
@@ -1,15 +1,35 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const jwt = require('jsonwebtoken');
 
 class UserController extends Controller {
   async register() {
     const { ctx } = this;
+    const { email, nickname, sign } = ctx.request.body;
+    const user = {
+      email,
+      nickname,
+      sign,
+    };
+    const ret = await ctx.service.user.create(user);
     ctx.body = 'hi, egg';
   }
   async login() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
+  }
+  async jwtSign(user) {
+    const { ctx, config } = this;
+    const token = jwt.sign(user, config.login.secretKey, {
+      expiresIn: config.login.expires,
+    });
+    const ret = ctx.msg.success;
+    ret.data = {
+      access_token: token,
+      expires_in: Math.floor(Date.now() / 1000) + this.config.login.expires,
+    };
+    ctx.body = ret;
   }
   async upload() {
     const { ctx } = this;
--- backend/app/service/user.js
+++ backend/app/service/user.js
@@ -3,7 +3,31 @@
 const Service = require('egg').Service;
 
 class UserService extends Service {
-  async create() {
+  async create(user) {
+    const { ctx } = this;
+    const nickname = await ctx.model.User.find({ where: { nickname: user.nickname } });
+    if (nickname !== null) {
+      return ctx.msg.nameUsed;
+    }
+    const userRow = await this.find(user.address);
+    if (userRow === null) {
+      const ret = ctx.msg.success;
+      const nowUnixTime = ctx.helper.nowUnixTime();
+      this.logger.info('user %j', user);
+      this.logger.info('ret %j', ret);
+      const createResult = await ctx.model.User.create({
+        ...user,
+        createTime: nowUnixTime,
+        lastLoginTime: nowUnixTime,
+        lastLoginIP: ctx.ip,
+      });
+      ret.data = createResult;
+      return ret;
+    }
+    return ctx.msg.userExists;
+  }
+  async find(id) {
+    return this.ctx.model.User.find({ where: { id } });
   }
 }
 
